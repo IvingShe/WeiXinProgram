8ea3dc5d0810b052b2d57db33db2b143906c2b25
IvingHome <249301352@qq.com>
2019-05-27 00:26:03 +0800

    First stage for Weixin Demo

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,9 +1,15 @@
 //app.js
 App({
   onLaunch: function () {
-    
+    var result = "sorry, it not works."
+    var date = new Date();
     console.log("Weixin first log:hello world!")
-    console.log("Weixin log for testing building automatically by ctrl+S")
+    console.log("Weixin log for testing building automatically by ctrl+S. ")
+    console.log(result)
+    var year = date.getFullYear;
+    var month = date.getMonth;
+    var day= date.getDay;
+    console.log(year + ":" + month + ":" + day)
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
@@ -13,5 +19,11 @@ App({
     }
 
     this.globalData = {}
+  },
+  onShow:function(){
+    console.log("----onShow----");
+  },
+  onHide:function(){
+    console.log("----onHide----");
   }
 })
